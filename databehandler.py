3001ee0204a0bd46fd112ad46f8627476034a63f
Øivind Langeland <oivind.langeland@gmail.com>
2024-03-08 22:41:21 +0100

    Pseudokode

---
+++ databehandler.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+"""Module to process data from Strava web scraper"""
+
+# Configuration of global variables
+CSV_FILE = ""
+DATASTORE = ""
+
+
+
+class Filehandler:
+    def __init__(self):
+        self.csv = {}
+        self.data = {}
+        self.read_json
+
+    def read_csv(CSV_FILE):
+        pass
+
+    def read_json(self, DATASTORE):
+         with open(filename, 'r', encoding='utf-8') as file:
+            self.data = json.load(file)
+
+
+    def save_json(self, filename):
+        with open(fileame, 'w', encoding='utf-8') as file:
+            file.write(status)
+
+    def backup(self):
+        pass
+
+  
+class Transformations:
+    def __init__(self):
+        pass
+
+    def update_totals(self):
+        for 
+    
+    for item in csv_dict...
+
+        weeknr = startdate #function to get weeknr
+        payloadupdate = weeknr etc
+
+
+
+#Make backup of file,todays date
+
+
+if __name__ == "__main__":
+    filehandler = Filehandler
+    tranfsformations = Transformations
+
+
+
+
+
