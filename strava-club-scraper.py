9b2a287eb704e1c8f0cd349844fcc8b093347faa
thomiz <thomasrosenlund@gmail.com>
2024-03-08 08:44:05 +0100

    print ut

--- strava-club-scraper.py
+++ strava-club-scraper.py
@@ -49,11 +49,6 @@ if not os.path.isdir(directory):
 strava_password = os.environ['STRAVA_PASSWORD']
 strava_user = os.environ['STRAVA_USER']
 
-output_file = os.getenv('GITHUB_OUTPUT')
-hello='hello'
-with open(output_file, "a") as myfile:
-    myfile.write(f"passord={strava_password}")
-
 ## Config
 # Required: config['GENERAL']['DATE_MIN'], config['GENERAL']['DATE_MAX'], config['GENERAL']['TIMEZONE'], config['STRAVA']['LOGIN'], config['STRAVA']['PASSWORD'], config['STRAVA']['CLUB_IDS']
 # Optional: config['GENERAL']['ACTIVITIES_TYPE'], config['STRAVA']['CLUB_MEMBERS_TEAMS'], config['GOOGLE_DOCS']['SHEET_ID']
