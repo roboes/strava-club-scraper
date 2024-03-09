0d92268fae8a3439a80900a040a89ca5af99d10c
Xivind <oivind.langeland@gmail.com>
2024-03-09 01:13:30 +0100

    Første versjon av databehandler-komponenten

--- databehandler.py
+++ databehandler.py
@@ -1,56 +1,68 @@
 #!/usr/bin/env python3
 """Module to process data from Strava web scraper"""
 
+import csv
+import json
+from datetime import datetime
+
 # Configuration of global variables
-CSV_FILE = ""
-DATASTORE = ""
-
-
+CSV_FILE = "testdata.csv"
+RESULTS_FILE = "results.json"
 
 class Filehandler:
+    """Class to process data and handle files"""
     def __init__(self):
-        self.csv = {}
-        self.data = {}
-        self.read_json
+        #self.backup()
+        self.datastore = {}
+        self.read_datastore()
+        self.process_csv()
 
-    def read_csv(CSV_FILE):
-        pass
+    def get_week_number(self, date_str):
+        """Method to translate weekday to week number"""
+        date_obj = datetime.strptime(date_str, '%Y-%m-%d')
+        week_number = date_obj.strftime('%G-W%V')
+        return week_number.split('-W')[1]
 
-    def read_json(self, DATASTORE):
-         with open(filename, 'r', encoding='utf-8') as file:
-            self.data = json.load(file)
+    def read_datastore(self):
+        """Method to read json data from file"""
+        with open(RESULTS_FILE, 'r', encoding='utf-8') as file:
+            self.datastore = json.load(file)
 
+    def process_csv(self):
+        """Method to read and process csv data from file"""
+        with open(CSV_FILE, 'r', encoding='utf-8') as file:
+            csv_list = list(csv.DictReader(file))
 
-    def save_json(self, filename):
-        with open(fileame, 'w', encoding='utf-8') as file:
-            file.write(status)
+        for dictionary in csv_list:
+            week_number = self.get_week_number(dictionary["leaderboard_date_start"])
+            athlete_weekly_uid = f'{week_number}-{dictionary["athlete_name"]}'
+            self.datastore.update({athlete_weekly_uid: {"week_number": week_number,
+                                   "athlete_name": dictionary["athlete_name"],
+                                   "activities": int(dictionary["activities"]),
+                                   "distance": int(float(dictionary["distance"]))/1000,
+                                   "distance_longest": int(float(dictionary["distance_longest"]))/1000,
+                                   "elevation_gain": int(float(dictionary["elevation_gain"]))
+                                   }})
 
-    def backup(self):
-        pass
+        self.save_datastore()
 
-  
+    def save_datastore(self):
+        """Method to write json data to file"""
+        with open(RESULTS_FILE, 'w', encoding='utf-8') as file:
+            json.dump(self.datastore, file, indent=2)
+
 class Transformations:
+    """Class to enrich datastore with aggregation etc"""
     def __init__(self):
         pass
 
     def update_totals(self):
-        for 
-    
-    for item in csv_dict...
-
-        weeknr = startdate #function to get weeknr
-        payloadupdate = weeknr etc
-
-
-
-#Make backup of file,todays date
-
+        """Method to update totals for a given period"""
 
 if __name__ == "__main__":
-    filehandler = Filehandler
-    tranfsformations = Transformations
 
+    filehandler = Filehandler()
+    #tranfsformations = Transformations() Maybe handle in script to create html file
 
-
-
-
+#Add basic error handling with output file
+#Make backup of file,todays date
