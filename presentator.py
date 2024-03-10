7059d31701c0b180e3ef461ec77788a89c60f5a8
Xivind <oivind.langeland@gmail.com>
2024-03-10 03:46:08 +0100

    La til funksjon for ranking

--- presentator.py
+++ presentator.py
@@ -6,7 +6,8 @@ import os
 from datetime import datetime
 
 # Configuration of global variables
-FILE_PATH = 'web/results.html'
+FILE_PATH = 'web/results.html'  # CHANGE BEFORE PUSHING TO REMOTE, FOR DEBUG
+# FILE_PATH = 'results.html' # CHANGE BEFORE PUSHING TO REMOTE, FOR DEBUG
 
 directory = 'web'
 if not os.path.isdir(directory):
@@ -23,15 +24,47 @@ def get_current_week_number():
     week_number = datetime.now().strftime('%G-W%V')
     return week_number.split('-W')[1]
 
+
 def format_duration(duration_minutes):
     hours = int((duration_minutes/60)/60)
     return hours
+
 
 def format_distance(distance):
     km = float(distance/1000)
     return km
 
 
+def get_ranking():
+    ranking_current_week = []
+    ranking_previous_week = []
+    rankings = {}
+    
+    for value in data.values():
+
+        if value["week_number"] == int(get_current_week_number()):
+            ranking_current_week.append(value["athlete_name"])
+
+        if value["week_number"] == int(get_current_week_number())-1:
+            ranking_previous_week.append(value["athlete_name"])
+    
+    for value in data.values():
+        if value["athlete_name"] in ranking_current_week and value["athlete_name"] in ranking_previous_week:
+            current_rank = ranking_current_week.index(value["athlete_name"])
+            previous_rank = ranking_previous_week.index(value["athlete_name"])
+
+            if current_rank < previous_rank:
+                rankings.update({value["athlete_name"]: "🔺"})
+            elif current_rank > previous_rank:
+                rankings.update({value["athlete_name"]: "🔻"})
+            elif current_rank == previous_rank:
+                rankings.update({value["athlete_name"]: "⏩"})
+        else:
+            rankings.update({value["athlete_name"]: "⭐"})
+
+    return rankings
+
+rankings = get_ranking()
 # Create a dictionary to store the accumulated data for each athlete
 athlete_summary = {}
 
@@ -52,8 +85,8 @@ for key, value in data.items():
     athlete_summary[athlete_name]['moving_time'] += value['moving_time']
     athlete_summary[athlete_name]['elevation_gain'] += value['elevation_gain']
 
-# Create HTML tables for each section
-ukens_resultater_table = "<table border='1'>\
+# Create HTML tables for each section - note, this way of doing this creates whitespace in html
+ukens_resultater_table = "<table class='table'>\
                          <tr><th>Navn</th>\
                          <th>Antall aktiviteter</th>\
                          <th>Varighet (timer)</th>\
@@ -63,7 +96,7 @@ ukens_resultater_table = "<table border='1'>\
 for key, value in data.items():
     if int(value["week_number"]) == int(get_current_week_number()):
         ukens_resultater_table += (
-            f"<tr><td>{value['athlete_name']}</td>"
+            f"<tr><td>{rankings[value['athlete_name']]} {value['athlete_name']}</td>"
             f"<td>{value['activities']}</td>"
             f"<td>{format_duration(value['moving_time'])}</td>"
             f"<td>{format_distance(value['distance'])}</td>"
@@ -71,7 +104,7 @@ for key, value in data.items():
         )
 ukens_resultater_table += "</table>"
 
-forrige_ukes_resultater_table = "<table border='1'>\
+forrige_ukes_resultater_table = "<table class='table'>\
                                 <tr><th>Navn</th>\
                                 <th>Antall aktiviteter</th>\
                                 <th>Varighet (timer)</th>\
@@ -89,7 +122,7 @@ for key, value in data.items():
         )
 forrige_ukes_resultater_table += "</table>"
 
-resultater_hele_perioden_table = "<table border='1'>\
+resultater_hele_perioden_table = "<table class='table'>\
                                  <tr><th>Navn</th>\
                                  <th>Antall aktiviteter</th>\
                                  <th>Varighet (timer)</th>\
@@ -113,26 +146,33 @@ html_content = f"""
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>My HTML Page</title>
+    <link rel="stylesheet" href="static/styles.css">
+    <title>Tittel</title>
 </head>
 <body>
-    <section id="Aggregrerte data">
-        Placeholder aggregerte data: Siste ukes vinner, totalt tid, distanse, høydemeter, co2?
-    </section>
-    <section id="ukens_resultater">
-        <h2>Ukens resultater (uke {int(get_current_week_number())})</h2>
-        {ukens_resultater_table}
-    </section>
+    <div class="page-wrapper">
+        <div class="header" id="header">
+            <h1 class=>Tittel</h1>
+        </div>
+        <div class="tile" id="Aggregrerte data">
+            <h2>Placeholder aggregerte data</h2>
+            Siste ukes vinner, totalt tid, distanse, høydemeter, co2?
+        </div>
+        <div class="tile" id="ukens_resultater">
+            <h2>Ukens resultater (uke {int(get_current_week_number())})</h2>
+            {ukens_resultater_table}
+        </div>
 
-    <section id="forrige_ukes_resultater">
-        <h2>Forrige ukes resultater (uke {int(get_current_week_number())-1})</h2>
-        {forrige_ukes_resultater_table}
-    </section>
+        <div class="tile" id="forrige_ukes_resultater">
+            <h2>Forrige ukes resultater (uke {int(get_current_week_number())-1})</h2>
+            {forrige_ukes_resultater_table}
+        </div>
 
-    <section id="resultater_hele_perioden">
-        <h2>Resultater hele perioden</h2>
-        {resultater_hele_perioden_table}
-    </section>
+        <div class="tile" id="resultater_hele_perioden">
+            <h2>Resultater hele perioden</h2>
+            {resultater_hele_perioden_table}
+        </div>
+    </div>
 </body>
 </html>
 """
