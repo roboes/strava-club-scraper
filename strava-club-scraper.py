9f90f51934418098be9996693d12cdb898dbc24e
roboes <rg.roberto@outlook.com>
2025-06-25 22:05:47 +0200

    Update

--- strava-club-scraper.py
+++ strava-club-scraper.py
@@ -102,6 +102,7 @@ def selenium_webdriver(*, web_browser='chrome', headless=False):
         webdriver_options.add_argument('--disable-search-engine-choice-screen')
         webdriver_options.add_argument('--log-level=3')
         # webdriver_options.add_argument('--disable-javascript')
+        # webdriver_options.ignore_local_proxy_environment_variables()
         webdriver_options.add_experimental_option(
             'prefs',
             {
